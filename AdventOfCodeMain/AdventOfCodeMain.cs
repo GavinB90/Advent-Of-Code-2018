acf895f899879b83359f5d37364b1308d7119373
GavinB90 <gbrooker90@gmail.com>
2018-12-05 21:11:40 +0000

    Day 1

---
+++ AdventOfCodeMain/AdventOfCodeMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCodeMain
+{
+    class AdventOfCodeMain
+    {
+        static void Main(string[] args)
+        {
+            Day1Part1 day1Part1 = new Day1Part1();
+        }
+    }
+}
